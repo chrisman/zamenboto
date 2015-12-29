3ef09587439ffd1d44a7a46a94a58767b10f6008
Christopher Brown <christopher.p.brown@gmail.com>
2015-12-29 09:49:09 -0700

    db checkpoint

--- data/databaseGenerator.js
+++ data/databaseGenerator.js
@@ -7,17 +7,31 @@ var fs = require('fs');
 var outputFile = process.argv[2] || path.resolve(__dirname, 'norrisbot.db');
 var db = new sqlite3.Database(outputFile);
 
-fs.readFile('data.json', function(error, body){
-  // db.serialize();
-  // // Creates the database structure
-  // db.run('CREATE TABLE IF NOT EXISTS info (name TEXT PRIMARY KEY, val TEXT DEFAULT NULL)');
-  // db.run('CREATE TABLE IF NOT EXISTS jokes (id INTEGER PRIMARY KEY, joke TEXT, used INTEGER DEFAULT 0)');
-  // db.run('CREATE INDEX jokes_used_idx ON jokes (used)');
+var onerr = function(err){
+  if (err) {
+    console.error('DATABASE ERROR', err);
+  }
+};
 
-  var data = JSON.parse(body.toString())["value"]
-  console.log(data);
-  var collector = Object.keys(data).map(function(i){
-    return data[i]["joke"];
+fs.readFile('data.json', function(error, body){
+  db.serialize();
+  // Creates the database structure
+  db.run('CREATE TABLE IF NOT EXISTS info (name TEXT PRIMARY KEY, val TEXT DEFAULT NULL)');
+  db.run('CREATE TABLE IF NOT EXISTS jokes (id INTEGER PRIMARY KEY, joke TEXT, used INTEGER DEFAULT 0)');
+  db.run('CREATE INDEX jokes_used_idx ON jokes (used)');
+
+  var data = JSON.parse(body.toString())["value"];
+  var result = data.filter(function(o){
+    return o.hasOwnProperty("joke");
   });
-  console.log(collector);
+
+  result.forEach(function(o){
+    for (var k in o) {
+      if (k === 'joke') {
+        db.run('INSERT INTO jokes (joke) VALUES (?)', o[k], onerr);
+      }
+    }
+  });
+
+  db.close();
 });
